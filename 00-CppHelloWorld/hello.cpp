3c6fe5fa60a9f34710cfeecf62f4530105679508
Tomás Herrero <82196148+tomas-herrero@users.noreply.github.com>
2021-04-16 00:46:36 -0300

    Update hello.cpp

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include <fstream>
 int main()
 
 {
-
-    std::ofstream myfile;
-    myfile.open("output.txt");
-
-    myfile << "Hello, World!\n";
-
-    myfile.close();
-
     std::cout << "Hello, World!\n";
-    return 0;
 }
