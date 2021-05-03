257315b3234bc6e4ff55ad67e243733a60ca40e7
Tomás Herrero <tomasherrero@frba.utn.edu.ar>
2021-05-03 18:16:05 -0300

    EjemploTipos

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -8,8 +8,12 @@ assert(true or false);
 
 assert((4+5)!=10);
 
-assert((10*14)>=(29%5)+100);
+assert((10*14)>=(30/5)+100);
 
 assert(not false and true);
+
+assert((1.25*4)/5==1);
+
+assert((628%5)==3 and (true!=(false or not true)));
 
 }
